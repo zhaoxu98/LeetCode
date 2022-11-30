36fb65f4fcc4477816a7f52ffe7f3bda69184546
zhaoxu <zhaohsu98@gmail.com>
2022-11-30 17:08:51 +0800

    lt 70

---
+++ 70.py
@@ -0,0 +1,8 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+        elif n == 2:
+            return 2
+        else:
+            return self.climbStairs(n - 1) + self.climbStairs(n - 2)
